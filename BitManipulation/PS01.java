3a36490f5ecdd51097ca5121dd644cea2d387135
sahilmishra03 <sahilmishra03032005@gmail.com>
2025-03-06 00:31:08 +0530

    06/03/25 4COMMIT

--- BitManipulation/PS01.java
+++ BitManipulation/PS01.java
@@ -4,5 +4,7 @@ public class PS01 {
     public static void main(String[] args) {
         System.out.println(5 & 6);
         System.out.println(5 | 6);
+        System.out.println(5 ^ 6);
+        System.out.println(~5);
     }
 }
