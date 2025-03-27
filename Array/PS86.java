342aa480d0f3446e96fee69e04abd9cc09b17ea9
sahilmishra03 <sahilmishra03032005@gmail.com>
2025-03-27 19:48:29 +0530

    new changes 3

--- Array/PS86.java
+++ Array/PS86.java
@@ -15,7 +15,7 @@ public class PS86 {
             char c2 = s.charAt(s.length() - 1);
 
             if ((c1 == 'a' || c1 == 'e' || c1 == 'i' || c1 == 'o' || c1 == 'u') &&
-                    (c2 == 'a' || c2 == 'e' || c2 == 'i' || c2 == 'o' || c2 == 'u')) {
+                (c2 == 'a' || c2 == 'e' || c2 == 'i' || c2 == 'o' || c2 == 'u')) {
                 count++;
             }
         }
