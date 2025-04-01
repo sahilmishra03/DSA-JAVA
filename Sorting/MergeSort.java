98a3a73335f48ff7b68bb10746231907b5fb507f
sahilmishra03 <sahilmishra03032005@gmail.com>
2025-04-01 23:15:00 +0530

    new changes 11

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
+        //TIME COMPLEXITY O(n*logn)
         int[] arr = { 3, 5, 2, 1, 6, 8 };
         System.out.println(Arrays.toString(mergeSort(arr)));
     }
