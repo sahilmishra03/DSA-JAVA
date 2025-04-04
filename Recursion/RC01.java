ecc0080f75b6c8c1e316d19d246645ef1441fb44
sahilmishra03 <sahilmishra03032005@gmail.com>
2025-04-04 23:36:19 +0530

    Recursion 2

--- Recursion/RC01.java
+++ Recursion/RC01.java
@@ -2,7 +2,7 @@ package Recursion;
 
 public class RC01 {
     public static void main(String[] args) {
-        fun2(5);
+        System.out.println(fun4(5));
     }
 
     static void fun1(int n) {
@@ -23,5 +23,24 @@ public class RC01 {
 
         System.out.println(n);
         fun2(n - 1);
+    }
+
+    static int fun3(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int smallAns = fun3(n - 1);
+
+        return n * smallAns;
+    }
+
+    static int fun4(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+
+        return fun4(n - 1) + fun4(n - 2);
     }
 }
