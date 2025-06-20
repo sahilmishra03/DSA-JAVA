9cdd6ff3e7f1a4a689552d7dab19df65ed4f6f6e
sahilmishra03 <sahilmishra03032005@gmail.com>
2025-06-20 21:02:45 +0530

    20-06-2025

--- LinkedList/PS01.java
+++ LinkedList/PS01.java
@@ -23,13 +23,10 @@ public class PS01 {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode tNode = head;
         HashSet<Integer> set = new HashSet<>();
-        // Step 1: Collect all unique values
         while (tNode != null) {
             set.add(tNode.val);
             tNode = tNode.next;
         }
-
-        // Step 2: Build a new linked list from the HashSet
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
         for (int val : set) {
