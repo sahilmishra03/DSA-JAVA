a5651ac0804142a635baea02b5419aad39608d04
Sahil Mishra <106021283+sahilmishra03@users.noreply.github.com>
2025-09-07 21:20:30 +0530

    07-09-25

--- Array/Methods.java
+++ Array/Methods.java
@@ -3,7 +3,7 @@ package Array;
 public class Methods {
     public static void main(String[] args) {
         int[] arr = new int[10];
-        arr[0]=0;
+        arr[0]=20;
         arr[1]=10;
         arr[2]=20;
         arr[3]=30;
