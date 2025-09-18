9f2cb71b0ded8f4d22e6fe8ad6cd6a7501700317
Sahil Mishra <106021283+sahilmishra03@users.noreply.github.com>
2025-09-18 22:06:26 +0530

    18-09-25

--- Array/PS177.java
+++ Array/PS177.java
@@ -25,12 +25,12 @@ public class PS177 {
             return nums.length - 1;
         }
 
-        for (int i = 0; i < nums.length-1; i++) {
-            if (nums[i]>nums[i+1]) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
                 return i;
             }
         }
 
-        return nums.length-1;
+        return nums.length - 1;
     }
 }
