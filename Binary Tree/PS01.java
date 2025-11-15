78a3f7eb1c77f5dd1028c3350032fb61344263dc
Sahil Mishra <106021283+sahilmishra03@users.noreply.github.com>
2025-11-15 16:52:05 +0530

    15-11-25

--- Binary Tree/PS01.java
+++ Binary Tree/PS01.java
@@ -1,7 +1,6 @@
 
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 public class PS01 {
@@ -15,32 +14,6 @@ public class PS01 {
             left = null;
             right = null;
         }
-    }
-
-    static ArrayList<Integer> list;
-
-    public static boolean searchBT(Node root, int val) {
-        if (root == null) {
-            return false;
-        }
-
-        if (root.val == val) {
-            list.add(root.val);
-            return true;
-        }
-
-        boolean leftSearch = searchBT(root.left, val);
-        if (leftSearch) {
-            list.add(root.val);
-            return true;
-        }
-
-        boolean rightSearch = searchBT(root.right, val);
-        if (rightSearch) {
-            list.add(root.val);
-            return true;
-        }
-        return false;
     }
 
     public static void kleveldown(Node root, int k) {
@@ -65,24 +38,6 @@ public class PS01 {
         }
     }
 
-    public static void pathHelper(Node root, int sum, List<String> out, int low, int high) {
-        if (root == null)
-            return;
-
-        if (root.left == null && root.right == null) {
-            sum += root.val;
-            if (sum >= low && sum <= high) {
-                out.add(root.val + "");
-            }
-            System.out.println(out);
-            return;
-        }
-
-        sum = sum + root.val;
-        pathHelper(root.left, sum, out, low, high);
-        pathHelper(root.right, sum, out, low, high);
-    }
-
     public static boolean pathsHelper(Node root, ArrayList<Integer> temp, int target) {
         if (root == null)
             return false;
@@ -105,8 +60,26 @@ public class PS01 {
         return false;
     }
 
+    public static void pathToLeafFromRootWithLowAndHigh(Node root, String path, int sum, int low, int high) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.left == null && root.right == null) {
+            sum += root.val;
+            path += root.val;
+            if (sum >= low && sum <= high) {
+                System.out.println(path);
+            }
+
+            return;
+        }
+
+        pathToLeafFromRootWithLowAndHigh(root.left, path + root.val + " ", sum + root.val, low, high);
+        pathToLeafFromRootWithLowAndHigh(root.right, path + root.val + " ", sum + root.val, low, high);
+    }
+
     public static void main(String[] args) {
-        list = new ArrayList<>();
         Node newTree = new Node(50);
         newTree.left = new Node(25);
         newTree.right = new Node(75);
@@ -117,8 +90,8 @@ public class PS01 {
         newTree.left.right.left = new Node(30);
         newTree.left.right.right = new Node(40);
         newTree.right.left.left = new Node(60);
-        newTree.right.left.left = new Node(70);
+        newTree.right.left.right = new Node(70);
 
-        pathHelper(newTree, 0, new ArrayList<>(), 100, 250);
+        pathToLeafFromRootWithLowAndHigh(newTree, "", 0, 150, 250);
     }
 }
